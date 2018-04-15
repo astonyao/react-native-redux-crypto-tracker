8725197c9b283c2cfd6c8c207fc4fe261a300036
Aston Yao <aston@fairdigital.com.au>
2018-04-15 19:20:56 +1000

    fetching with axios

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
+
 import Store from './src/Store';
 import { Header, CryptoContainer } from './src/components';
 
-export default class App extends React.Component {
+
+export default class App extends Component {
   render() {
     return (
       <Provider store={Store}>
@@ -16,12 +18,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
--- src/components/CryptoContainer.js
+++ src/components/CryptoContainer.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { View, Text } from 'react-native';
 
+import FetchCoinData from './../Actions/FetchCoinData';
+
 
 
 class CryptoContainer extends Component {
+
+    componentDidMount(){
+        this.props.FetchCoinData();
+    }
+
     render() {
         return (
             <View>
@@ -21,4 +28,4 @@ function mapStateToProps(state) {
 
 }
 
-export default connect(mapStateToProps)(CryptoContainer)
+export default connect(mapStateToProps, { FetchCoinData })(CryptoContainer)
